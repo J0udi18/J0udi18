d8a7340ef5000cd19014b6ad98544ea7c5d19879
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2022-03-23 10:52:12 +1300

    upload file

---
+++ 02_variables.py
@@ -0,0 +1,14 @@
+# code to learn about variables
+
+# variables can be a string  
+greeting = "hello world"
+
+# they can also be numbers 
+fav_num = 7
+
+# this variables has a problem
+my_name = "Joudi alkak"
+
+print(greeting)
+print(fav_num)
+print(my_name)
